9f3c022b22b6d5194dcde02dea34a71e128d5ae0
Angelin-Bonnet Olivia <oangelinbo@bs406-s16-08.insa-lyon.fr>
2014-12-10 17:02:40 +0100

    Add function resize (student 2)

--- Teststring.cpp
+++ Teststring.cpp
@@ -40,12 +40,18 @@ int main(int argc, char* argv[])
   bool emptyornot;
   string my_string= string("Google");
   my_string.display();
+
   // test method empty
   emptyornot = my_string.empty();
 
   printf("%d\n",emptyornot);
 
   string my_string_copy=string(my_string);
+  my_string_copy.display();
+
+//Test for resize
+  printf("Length of my_string_copy: %ld\n", my_string.length());
+  my_string_copy.resize(4);
   my_string_copy.display();
 
   printf("Length of my_string_copy: %ld\n", my_string_copy.length());
